fd80aee61ba442a252e34a968b5e32ec94b75e76
Luís Gustavo de Oliveira Miranda <55678707+Persapius@users.noreply.github.com>
2023-08-17 21:09:54 -0300

    Update LeituraSensorDeUmidade.py

--- LeituraSensorDeUmidade.py
+++ LeituraSensorDeUmidade.py
@@ -18,11 +18,11 @@ def ler_dados_do_serial(porta_serial):
                 valores = linha.split(',')  # Separa os valores usando a vírgula como delimitador (CSV)
                 dados.append(valores) # Adicionando os valores dos sensores aos dados
                 print(valores)  # Apenas para verificar os dados na saída do console
-            if len(dados) >= 5000:  # Para o exemplo, vamos salvar 1000 linhas de dados em CSV
+            if len(dados) >= 1000:  # Para o exemplo, vamos salvar 1000 linhas de dados em CSV
                 break
     return dados
 
 if __name__ == '__main__':
     porta_serial_arduino = '/dev/ttyUSB0'  # Substitua pelo nome da porta serial do Arduino no seu sistema
     dados_recebidos = ler_dados_do_serial(porta_serial_arduino) # Fazer a leitura dos dados seriais do arduino
-    salvar_dados_em_csv(dados_recebidos, 'dados_do_arduino_Novos.csv') # Salvando os arquivos em CSV
+    salvar_dados_em_csv(dados_recebidos, 'dados_do_arduino.csv') # Salvando os arquivos em CSV
